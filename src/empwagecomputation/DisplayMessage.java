c1c9e2dd71afbb7060451b215347060921274edd
Rakesh <byreddyrakesh41@gmail.com>
2022-10-17 11:35:06 +0530

    ADDED: Welcome To Employee Message

---
+++ src/empwagecomputation/DisplayMessage.java
@@ -0,0 +1,8 @@
+package empwagecomputation;
+
+public class DisplayMessage{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+
+    }
+}
